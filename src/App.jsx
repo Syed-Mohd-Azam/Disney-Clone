73ec606e7c8580863fea74e31f8bcdfe65580d1f
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-21 17:25:03 +0530

    Images Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import "./App.css";
 function App() {
   return (
     <>
-      <section></section>
+      <section className="w-full min-h-screen bg-black"></section>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Profile from "/";
+const Header = () => {
+  return (
+    <>
+      <section>
+        <article></article>
+        <article>
+          <img src="" alt="Profile-Image" />
+        </article>
+      </section>
+    </>
+  );
+};
+export default Header;
