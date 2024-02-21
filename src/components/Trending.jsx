7025e181f1b9703dcf527ba1abe213bd0a52e04b
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-22 03:26:17 +0530

    Changes Added

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -33,24 +33,24 @@ const Trending = () => {
         className=" w-full flex flex-row px-16 py-5 overflow-hidden  scrollbar-hide scroll-smooth"
         ref={elementRef}
       >
-        <article className="absolute top-1/2 left-8">
+        <article className="hidden lg:block lg:absolute top-1/2 left-7">
           <FaLessThan
-            className="text-black  bg-white w-8 h-8 p-2 rounded-md hover:cursor-pointer "
+            className="text-white w-9 h-9 p-2 rounded-md hover:cursor-pointer font-bold"
             style={{ position: "absolute" }}
             onClick={() => scrollLeft(elementRef.current)}
           />
         </article>
         {movies?.map((movie) => (
           <img
-            className="min-w-full h-[300px] object-fill  mr-12 rounded-lg"
+            className="min-w-full md:h-[300px] object-fill  mr-12 rounded-lg"
             key={movie?.id}
             src={POSTER_PATH_URL + movie?.backdrop_path}
             alt="Trending-Banner-Image"
           />
         ))}
-        <article className="absolute top-1/2 right-10">
+        <article className="hidden lg:block  lg:absolute top-1/2 right-14">
           <FaGreaterThan
-            className="text-black  bg-white w-8 h-8 p-2 rounded-md hover:cursor-pointer z-50"
+            className="text-white w-9 h-9 p-2 rounded-md hover:cursor-pointer z-50 font-bold"
             style={{ position: "absolute" }}
             onClick={() => scrollRight(elementRef.current)}
           />
