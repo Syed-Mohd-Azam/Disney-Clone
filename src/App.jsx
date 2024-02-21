cdf2f8ef5dbb98ce23c404ef14eea2aeab588984
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-21 17:48:38 +0530

    Disney-Logo and Profile-Logo Added to the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <section className="w-full min-h-screen bg-black"></section>
+      <section className="w-full min-h-screen bg-black">
+        <Header />
+      </section>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import Profile from "/";
+import Profile from "../assets/Profile-Image.png";
+import Disney from "../assets/Disney+_Image.png";
 const Header = () => {
   return (
     <>
-      <section>
-        <article></article>
+      <section className="flex items-center justify-between">
+        <article className="flex items-center gap-4 ">
+          <img
+            className="min-w-8 min-h-8 w-[120px]"
+            src={Disney}
+            alt="Disney-Image"
+          />
+          <section className="flex gap-2 items-center"></section>
+        </article>
         <article>
-          <img src="" alt="Profile-Image" />
+          <img
+            className="min-w-8 min-h-8 w-[70px] h-[70px] rounded-full"
+            src={Profile}
+            alt="Profile-Image"
+          />
         </article>
       </section>
     </>
