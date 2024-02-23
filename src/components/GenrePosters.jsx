29e2a54c5e9b7354f05c05c767bce7d846db115e
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-23 23:51:25 +0530

    Absolute Position Issue

--- src/components/GenrePosters.jsx
+++ src/components/GenrePosters.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 // eslint-disable-next-line react/prop-types, no-unused-vars
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PARTICULAR_GENRE_VIDEOS } from "../utils/constants";
 import MoviePoster from "./MoviePoster";
-const GenreVideos = ({ id }) => {
+import { FaLessThan } from "react-icons/fa6";
+import { FaGreaterThan } from "react-icons/fa6";
+const GenrePosters = ({ id }) => {
+  const myRef = useRef();
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     moviesList();
@@ -14,17 +17,38 @@ const GenreVideos = ({ id }) => {
     const json = await data.json();
     setMovies(json?.results);
   };
+  const scrollLeft = () => {
+    if (myRef.current) {
+      myRef.current.scrollLeft -= 500;
+    }
+  };
+  const scrollRight = () => {
+    if (myRef.current) {
+      myRef.current.scrollLeft += 500;
+    }
+  };
   if (movies?.length === 0) {
     return;
   }
   return (
     <>
-      <section className="flex flex-row overflow-x-auto gap-4 scrollbar-hide scroll-smooth transition-all ease-in-out duration-300">
+      <section
+        className="flex flex-row overflow-x-auto gap-4 scrollbar-hide scroll-smooth transition-all ease-in-out duration-300 relative"
+        ref={myRef}
+      >
+        <FaLessThan
+          className="text-white  w-[50px] h-[50px] absolute top-1/2 left-6 hover:cursor-pointer "
+          onClick={scrollLeft}
+        />
         {movies?.map((movie) => (
           <MoviePoster key={movie?.id} movie={movie} />
         ))}
+        <FaGreaterThan
+          className="text-white  w-[50px] h-[50px] absolute top-1/2 right-5 hover:cursor-pointer"
+          onClick={scrollRight}
+        />
       </section>
     </>
   );
 };
-export default GenreVideos;
+export default GenrePosters;
