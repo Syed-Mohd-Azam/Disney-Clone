6b947293198046a3dc43ec59a6428e62621ccf8c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-25 18:07:18 +0530

    Project Completed

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -4,7 +4,7 @@ import Production from "./Production";
 const ProductionHouse = () => {
   return (
     <>
-      <section className="flex flex-row w-11/12 mx-auto gap-3 sm:gap-4 md:gap-10 px-10">
+      <section className=" hidden sm:flex  flex-row w-11/12 mx-auto gap-2 sm:gap-4 md:gap-10 px-10">
         {productionHouse?.map(({ id, video, img }) => (
           <>
             <Production key={id} img={img} video={video} />
